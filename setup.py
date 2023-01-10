f6dc6f3e5f14c2b4f1aa4e5ec5974fc8446b2920
Niels Mündler <n.muendler@posteo.de>
2023-01-11 00:40:58 +0100

    This version is only valid for python3.8

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
     keywords="python cardano smart contract blockchain verification haskell",
     python_requires=">=3",
