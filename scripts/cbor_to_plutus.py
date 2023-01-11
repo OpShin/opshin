c2a7f51e120c027b6ab76606f86d138971956b0c
Niels Mündler <n.muendler@posteo.de>
2023-01-11 18:21:18 +0100

    Fix plutus script version

--- scripts/cbor_to_plutus.py
+++ scripts/cbor_to_plutus.py
@@ -6,8 +6,8 @@ inp = stdin.buffer.read()
 cbor = cbor2.dumps(inp)
 cbor_hex = cbor.hex()
 d = {
-    "type": "PlutusScriptV1",
-    "description": "A Smart Contract written in Eopsin",
+    "type": "PlutusScriptV2",
+    "description": "Eopsin Smart Contract",
     "cborHex": cbor_hex,
 }
 print(json.dumps(d))
