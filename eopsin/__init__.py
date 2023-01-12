1e4ddbf46fee200626884e65dcf4e54f81ffcc5d
Niels Mündler <n.muendler@posteo.de>
2023-01-12 12:00:52 +0100

    Add fixed script version

--- eopsin/__init__.py
+++ eopsin/__init__.py
@@ -8,7 +8,7 @@ try:
 except ImportError as e:
     warnings.warn(ImportWarning(e))
 
-VERSION = (0, 3, 9)
+VERSION = (0, 3, 10)
 
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
