86dd38c2546092bd07fff5ad46d36d225b3bdb1a
Niels Mündler <n.muendler@posteo.de>
2023-01-17 23:51:08 +0100

    Re-add universal minting policy

---
+++ examples/smart_contracts/universal_minting_policy.py
@@ -0,0 +1,41 @@
+from eopsin.prelude import *
+
+
+@dataclass()
+class Mint(PlutusData):
+    CONSTR_ID = 0
+    ref: TxInInfo
+
+
+@dataclass()
+class Burn(PlutusData):
+    CONSTR_ID = 1
+
+
+# Set parameter before re-compiling
+# eopsin contracts don't natively support parameterization
+precNFT = b""
+
+
+def validator(dn: int, redeemer: Union[Mint, Burn], ctx: ScriptContext) -> None:
+    purpose = ctx.purpose
+    if isinstance(purpose, Minting):
+        cs = purpose.currency_symbol
+
+        if isinstance(redeemer, Mint):
+            tokenNameTail = sha256(
+                redeemer.ref.out_ref.id + bytes([redeemer.ref.out_ref.idx])
+            ).digest()[dn:]
+            assert redeemer.ref in ctx.tx_info.inputs, "Reference UTxO not consumed"
+            mintPrecNFT = ctx.tx_info.mint[precNFT]
+            for name, amount in ctx.tx_info.mint[cs].items():
+                assert name.endswith(tokenNameTail), "Wrong tokenname"
+                assert amount == 1, "Wrong token amount"
+                assert (
+                    precNFT == b"" or mintPrecNFT == amount
+                ), "OwnNFT should be minted"
+        elif isinstance(redeemer, Burn):
+            for _, amount in ctx.tx_info.mint[cs].items():
+                assert amount == -1, "Own currency needs to be burnt"
+    else:
+        assert False, "Purpose of mint call must be minting"
