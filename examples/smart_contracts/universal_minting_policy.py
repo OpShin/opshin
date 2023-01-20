c9fd3e52740d58dd6cdd8fcb6c187a3338bcc4a0
Niels Mündler <n.muendler@posteo.de>
2023-01-20 01:41:04 +0100

    Update contract to use less missing features

--- examples/smart_contracts/universal_minting_policy.py
+++ examples/smart_contracts/universal_minting_policy.py
@@ -20,22 +20,26 @@ precNFT = b""
 def validator(dn: int, redeemer: Union[Mint, Burn], ctx: ScriptContext) -> None:
     purpose = ctx.purpose
     if isinstance(purpose, Minting):
-        cs = purpose.currency_symbol
+        cs = purpose.policy_id
 
         if isinstance(redeemer, Mint):
             tokenNameTail = sha256(
                 redeemer.ref.out_ref.id + bytes([redeemer.ref.out_ref.idx])
             ).digest()[dn:]
-            assert redeemer.ref in ctx.tx_info.inputs, "Reference UTxO not consumed"
-            mintPrecNFT = ctx.tx_info.mint[precNFT]
-            for name, amount in ctx.tx_info.mint[cs].items():
+            ref_consumed = False
+            for tx_in in ctx.tx_info.inputs:
+                if redeemer.ref == tx_in:
+                    ref_consumed = True
+            assert ref_consumed, "Reference UTxO not consumed"
+            mintPrecNFT = ctx.tx_info.mint.get(cs, {b"": 0}).get(precNFT, 0)
+            for name, amount in ctx.tx_info.mint.get(cs, {b"": 0}).items():
                 assert name.endswith(tokenNameTail), "Wrong tokenname"
                 assert amount == 1, "Wrong token amount"
                 assert (
                     precNFT == b"" or mintPrecNFT == amount
                 ), "OwnNFT should be minted"
         elif isinstance(redeemer, Burn):
-            for _, amount in ctx.tx_info.mint[cs].items():
+            for _, amount in ctx.tx_info.mint.get(cs, {b""}).items():
                 assert amount == -1, "Own currency needs to be burnt"
     else:
         assert False, "Purpose of mint call must be minting"
