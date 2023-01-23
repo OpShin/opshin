91c8414c203f4ae4878b485127c8e93c378db603
Niels Mündler <n.muendler@posteo.de>
2023-01-23 18:36:16 +0100

    Bump version

--- eopsin/__init__.py
+++ eopsin/__init__.py
@@ -8,7 +8,7 @@ try:
 except ImportError as e:
     warnings.warn(ImportWarning(e))
 
-VERSION = (0, 7, 2)
+VERSION = (0, 7, 3)
 
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
