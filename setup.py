28cbdf96b0beb98eae0294078f65efa11ca45985
Niels Mündler <n.muendler@posteo.de>
2023-01-30 09:55:40 +0100

    Add python3.7 to test matrix

--- setup.py
+++ setup.py
@@ -47,10 +47,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     keywords="python cardano smart contract blockchain verification haskell",
-    python_requires=">=3.8, <3.9",
+    python_requires=">=3.7, <3.9",
     test_suite="eopsin.tests",
     entry_points={
         "console_scripts": ["eopsin=eopsin.__main__:main"],
