d7bd7e5734ebac3c3a8fb5a0352f1a954e47da87
Niels Mündler <n.muendler@posteo.de>
2023-02-03 15:14:31 +0100

    Fix copyright notice

--- eopsin/__init__.py
+++ eopsin/__init__.py
@@ -13,6 +13,6 @@ VERSION = (0, 7, 8)
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
 __author_email__ = "n.muendler@web.de"
-__copyright__ = "Copyright (C) 2019 nielstron"
+__copyright__ = "Copyright (C) 2023 nielstron"
 __license__ = "MIT"
 __url__ = "https://github.com/imperatorlang/eopsin"
