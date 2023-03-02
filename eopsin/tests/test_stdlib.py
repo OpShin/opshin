ce90e0c99252d01bc81da7fca16395a0fdd09895
Niels Mündler <n.muendler@posteo.de>
2023-03-02 11:34:25 +0100

    Add tests for literal expressions

--- eopsin/tests/test_stdlib.py
+++ eopsin/tests/test_stdlib.py
@@ -92,3 +92,55 @@ def validator(x: bytes) -> str:
         except UnicodeDecodeError:
             ret = None
         self.assertEqual(ret, exp, "bytes.decode returned wrong value")
+
+    @given(xs=st.binary())
+    @example(b"dc315c289fee4484eda07038393f21dc4e572aff292d7926018725c2")
+    def test_constant_bytestring(self, xs):
+        # this tests that errors that are caused by assignments are actually triggered at the time of assigning
+        source_code = f"""
+def validator(x: None) -> bytes:
+    return {xs}
+            """
+        ast = compiler.parse(source_code)
+        code = compiler.compile(ast)
+        code = code.compile()
+        f = code.term
+        # UPLC lambdas may only take one argument at a time, so we evaluate by repeatedly applying
+        for d in [uplc.BuiltinUnit()]:
+            f = uplc.Apply(f, d)
+        ret = uplc_eval(f).value
+        self.assertEqual(ret, xs, "literal bytes returned wrong value")
+
+    @given(xs=st.integers())
+    def test_constant_integer(self, xs):
+        # this tests that errors that are caused by assignments are actually triggered at the time of assigning
+        source_code = f"""
+def validator(x: None) -> int:
+    return {xs}
+            """
+        ast = compiler.parse(source_code)
+        code = compiler.compile(ast)
+        code = code.compile()
+        f = code.term
+        # UPLC lambdas may only take one argument at a time, so we evaluate by repeatedly applying
+        for d in [uplc.BuiltinUnit()]:
+            f = uplc.Apply(f, d)
+        ret = uplc_eval(f).value
+        self.assertEqual(ret, xs, "literal integer returned wrong value")
+
+    @given(xs=st.text())
+    def test_constant_string(self, xs):
+        # this tests that errors that are caused by assignments are actually triggered at the time of assigning
+        source_code = f"""
+def validator(x: None) -> str:
+    return {xs}
+            """
+        ast = compiler.parse(source_code)
+        code = compiler.compile(ast)
+        code = code.compile()
+        f = code.term
+        # UPLC lambdas may only take one argument at a time, so we evaluate by repeatedly applying
+        for d in [uplc.BuiltinUnit()]:
+            f = uplc.Apply(f, d)
+        ret = uplc_eval(f).value.decode("utf8")
+        self.assertEqual(ret, xs, "literal string returned wrong value")
