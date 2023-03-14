1dab385c26ba6109e303dfcc9aaa9a17dbd578f0
Niels Mündler <n.muendler@posteo.de>
2023-03-14 16:56:26 +0100

    Bump the package version
    - fixed versioning and name confusion in poetry
    - add ledger library

--- eopsin/__init__.py
+++ eopsin/__init__.py
@@ -10,3 +10,10 @@ try:
     from compiler import *
 except ImportError as e:
     warnings.warn(ImportWarning(e))
+
+__version__ = "0.9.12"
+__author__ = "nielstron"
+__author_email__ = "n.muendler@web.de"
+__copyright__ = "Copyright (C) 2023 nielstron"
+__license__ = "MIT"
+__url__ = "https://github.com/imperatorlang/eopsin"
