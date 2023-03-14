4b1eb9eccd8c175b36a07e9518d1108c0ad6158c
Niels Mündler <n.muendler@posteo.de>
2023-03-14 16:41:38 +0100

    Make pyproject.toml the single source of truth for the project version

--- eopsin/__init__.py
+++ eopsin/__init__.py
@@ -10,12 +10,3 @@ try:
     from compiler import *
 except ImportError as e:
     warnings.warn(ImportWarning(e))
-
-VERSION = (0, 9, 11)
-
-__version__ = ".".join([str(i) for i in VERSION])
-__author__ = "nielstron"
-__author_email__ = "n.muendler@web.de"
-__copyright__ = "Copyright (C) 2023 nielstron"
-__license__ = "MIT"
-__url__ = "https://github.com/imperatorlang/eopsin"
