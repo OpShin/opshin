880796d793c2d978b60012556f976ae686323501
Niels Mündler <n.muendler@posteo.de>
2023-03-15 13:28:40 +0100

    Improve documentation

--- build_docs.sh
+++ build_docs.sh
@@ -2,5 +2,5 @@
 set -e
 
 pip install pdoc3
-rm -r docs/eopsin
-pdoc --html eopsin -o docs
+rm -r docs/eopsin || echo "docs/eopsin was missing, that's ok"
+pdoc --html eopsin -o docs --template-dir docs
