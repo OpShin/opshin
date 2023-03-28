bfb5730908ef8d4f3c37f1950317349b4de42fb2
Niels Mündler <n.muendler@posteo.de>
2023-03-28 09:02:05 +0200

    Add int from bytes

--- opshin/tests/test_std/test_math.py
+++ opshin/tests/test_std/test_math.py
@@ -1,7 +1,7 @@
 import hypothesis
 import hypothesis.strategies as hst
 
-from opshin.std import fractions as oc_fractions
+from opshin.std import math as oc_math
 
 import math
 
@@ -10,9 +10,16 @@ pos_int = hst.integers(min_value=0)
 
 @hypothesis.given(pos_int, pos_int)
 def test_gcd(a: int, b: int):
-    assert oc_fractions.gcd(a, b) == math.gcd(a, b), "Invalid gcd"
+    assert oc_math.gcd(a, b) == math.gcd(a, b), "Invalid gcd"
 
 
-@hypothesis.given(hst.integers(), hst.integers())
-def test_sign(a: int, b: int):
-    assert oc_fractions.sign(a) == math.copysign(1, a), "Invalid sign"
+@hypothesis.given(hst.integers())
+def test_sign(a: int):
+    assert oc_math.sign(a) == math.copysign(1, a), "Invalid sign"
+
+
+@hypothesis.given(hst.binary())
+def test_unsigned_int_from_bytes_big(b: bytes):
+    assert oc_math.unsigned_int_from_bytes_big(b) == int.from_bytes(
+        b, byteorder="big", signed=False
+    ), "Invalid from bytes"
