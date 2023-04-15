e4707e67c21172e96caa8b0adb449175aaf9ede0
Niels Mündler <n.muendler@posteo.de>
2023-04-16 00:17:52 +0200

    Update docs

--- docs/index.js
+++ docs/index.js
@@ -1873,6 +1873,12 @@ INDEX=[
 "func":1
 },
 {
+"ref":"opshin.tests.test_misc.MiscTest.test_opt_unsafe_cast",
+"url":4,
+"doc":"",
+"func":1
+},
+{
 "ref":"opshin.tests.test_ops",
 "url":5,
 "doc":""
@@ -4075,6 +4081,17 @@ INDEX=[
 "ref":"opshin.rewrite.rewrite_import.import_module",
 "url":37,
 "doc":"An approximate implementation of import.",
+"func":1
+},
+{
+"ref":"opshin.rewrite.rewrite_import.RewriteLocation",
+"url":37,
+"doc":"A :class: NodeVisitor subclass that walks the abstract syntax tree and allows modification of nodes. The  NodeTransformer will walk the AST and use the return value of the visitor methods to replace or remove the old node. If the return value of the visitor method is  None , the node will be removed from its location, otherwise it is replaced with the return value. The return value may be the original node in which case no replacement takes place. Here is an example transformer that rewrites all occurrences of name lookups ( foo ) to  data['foo']  class RewriteName(NodeTransformer): def visit_Name(self, node): return Subscript( value=Name(id='data', ctx=Load( , slice=Index(value=Str(s=node.id , ctx=node.ctx ) Keep in mind that if the node you're operating on has child nodes you must either transform the child nodes yourself or call the :meth: generic_visit method for the node first. For nodes that were part of a collection of statements (that applies to all statement nodes), the visitor may also return a list of nodes rather than just a single node. Usually you use the transformer like this node = YourTransformer().visit(node)"
+},
+{
+"ref":"opshin.rewrite.rewrite_import.RewriteLocation.visit",
+"url":37,
+"doc":"Visit a node.",
 "func":1
 },
 {
