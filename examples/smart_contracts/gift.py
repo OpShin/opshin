b431859b143f7d6c3dbdd65764b81993237800ae
Niels Mündler <n.muendler@posteo.de>
2023-04-21 01:55:57 +0200

    Fix gift contract

--- examples/smart_contracts/gift.py
+++ examples/smart_contracts/gift.py
@@ -3,9 +3,9 @@ from opshin.prelude import *
 
 @dataclass
 class WithdrawDatum(PlutusData):
-    pubkeyhash: bytes
+    pubkeyhash: PubKeyHash
 
 
 def validator(datum: WithdrawDatum, redeemer: None, context: ScriptContext) -> None:
-    sig_present = datum in context.tx_info.signatories
+    sig_present = datum.pubkeyhash in context.tx_info.signatories
     assert sig_present, "Required signature missing"
