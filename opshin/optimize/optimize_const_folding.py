ea576206a2ac1c77ec4a0f58b64dc1ed9d6f4004
Niels Mündler <n.muendler@posteo.de>
2023-04-24 17:40:20 +0200

    Restrict to a subset of all globals

--- opshin/optimize/optimize_const_folding.py
+++ opshin/optimize/optimize_const_folding.py
@@ -5,7 +5,8 @@ try:
 except NameError:
     from astunparse import unparse
 
-from ..util import CompilingNodeTransformer
+from ..util import CompilingNodeTransformer, CompilingNodeVisitor
+from ..type_inference import INITIAL_SCOPE
 
 """
 Pre-evaluates constant statements
@@ -19,9 +20,98 @@ ACCEPTED_ATOMIC_TYPES = [
     bool,
 ]
 
+SAFE_GLOBALS_LIST = [
+    abs,
+    aiter,
+    all,
+    any,
+    anext,
+    ascii,
+    bin,
+    bool,
+    bytes,
+    bytearray,
+    callable,
+    chr,
+    classmethod,
+    compile,
+    complex,
+    delattr,
+    dict,
+    dir,
+    divmod,
+    enumerate,
+    filter,
+    float,
+    format,
+    frozenset,
+    getattr,
+    hasattr,
+    hash,
+    hex,
+    id,
+    input,
+    int,
+    isinstance,
+    issubclass,
+    iter,
+    len,
+    list,
+    map,
+    max,
+    min,
+    next,
+    object,
+    oct,
+    open,
+    ord,
+    pow,
+    print,
+    property,
+    range,
+    repr,
+    reversed,
+    round,
+    set,
+    setattr,
+    slice,
+    sorted,
+    staticmethod,
+    str,
+    sum,
+    super,
+    tuple,
+    type,
+    vars,
+    zip,
+]
+SAFE_GLOBALS = {x.__name__: x for x in SAFE_GLOBALS_LIST}
+
+
+class ShallowNameDefCollector(CompilingNodeVisitor):
+    step = "Collecting occuring variable names"
+
+    def __init__(self):
+        self.vars = set()
+
+    def visit_Name(self, node: Name) -> None:
+        if isinstance(node.ctx, Store):
+            self.vars.add(node.id)
+
+    def visit_ClassDef(self, node: ClassDef):
+        self.vars.add(node.name)
+        # ignore the content (i.e. attribute names) of class definitions
+
+    def visit_FunctionDef(self, node: FunctionDef):
+        self.vars.add(node.name)
+        # ignore the recursive stuff
+
 
 class OptimizeConstantFolding(CompilingNodeTransformer):
     step = "Constant folding"
+
+    def __init__(self):
+        scopes = [INITIAL_SCOPE]
 
     def generic_visit(self, node: AST):
         node = super().generic_visit(node)
@@ -37,7 +127,7 @@ class OptimizeConstantFolding(CompilingNodeTransformer):
             return node
         try:
             # TODO we can add preceding plutusdata definitions here!
-            node_eval = eval(node_source, globals(), {})
+            node_eval = eval(node_source, SAFE_GLOBALS, {})
         except Exception as e:
             return node
 
