9100773049c9f98ec4d4291cc29bd9e306249fd9
Niels Mündler <n.muendler@posteo.de>
2023-06-19 22:13:16 +0900

    Add a simple implementation of an efficient bitmap

---
+++ opshin/std/bitmap.py
@@ -0,0 +1,33 @@
+BitMap = bytes
+
+BYTE_SIZE = 8
+POWS = [2**i for i in range(BYTE_SIZE)]
+
+
+def init_bitmap(size: int):
+    return b"\x00" * (size // BYTE_SIZE)
+
+
+def isset_bitmap(bmp: BitMap, i: int) -> bool:
+    byte = bmp[i // BYTE_SIZE]
+    bit = (byte // POWS[i % BYTE_SIZE]) % 2
+    return bit == 1
+
+
+def set_bitmap(bmp: BitMap, i: int, v: bool) -> BitMap:
+    """
+    Sets a bit in the bitmap to the specified value
+    i: index of the value to set
+    v: value of the bit to be set (0 or 1)
+    """
+    byte = bmp[i // BYTE_SIZE]
+    bit = (byte // POWS[i % BYTE_SIZE]) % 2
+    if bit == v:
+        new_byte = byte
+    elif not v:
+        # v == 0
+        new_byte = byte - POWS[i]
+    else:
+        # v == 1
+        new_byte = byte + POWS[i]
+    return (bmp[: i - 1] if i > 0 else b"") + bytes([new_byte]) + bmp[i + 1 :]
