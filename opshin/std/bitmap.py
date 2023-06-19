1c9cd3ab8115d52811c09003e73f135c8f1a7391
Niels Mündler <n.muendler@posteo.de>
2023-06-19 23:55:55 +0900

    Add auxiliary helper functions

--- opshin/std/bitmap.py
+++ opshin/std/bitmap.py
@@ -5,16 +5,18 @@ POWS = [2**i for i in range(BYTE_SIZE)]
 
 
 def init_bitmap(size: int):
+    """Creates an empty bitmap with size bits"""
     return b"\x00" * ((size + BYTE_SIZE - 1) // BYTE_SIZE)
 
 
-def isset_bitmap(bmp: BitMap, i: int) -> bool:
+def test_bitmap(bmp: BitMap, i: int) -> bool:
+    """Tests if bit at position i has been set to 1"""
     byte = bmp[i // BYTE_SIZE]
     bit = (byte // POWS[(BYTE_SIZE - 1) - (i % BYTE_SIZE)]) % 2
     return bit == 1
 
 
-def set_bitmap(bmp: BitMap, i: int, v: bool) -> BitMap:
+def _set_bitmap(bmp: BitMap, i: int, v: bool) -> BitMap:
     """
     Sets a bit in the bitmap to the specified value
     i: index of the value to set
@@ -33,3 +35,48 @@ def set_bitmap(bmp: BitMap, i: int, v: bool) -> BitMap:
         # v == 1
         new_byte = byte + powi
     return bmp[:scaled_i] + bytes([new_byte]) + bmp[scaled_i + 1 :]
+
+
+def set_bitmap(bmp: BitMap, i: int) -> BitMap:
+    """Sets a bit in the bitmap to 1"""
+    return _set_bitmap(bmp, i, True)
+
+
+def reset_bitmap(bmp: BitMap, i: int) -> BitMap:
+    """Sets a bit in the bitmap to 0"""
+    return _set_bitmap(bmp, i, False)
+
+
+def flip_bitmap(bmp: BitMap, i: int) -> BitMap:
+    """Flips a bit in the bitmap"""
+    scaled_i = i // BYTE_SIZE
+    byte = bmp[scaled_i]
+    powi = POWS[(BYTE_SIZE - 1) - (i % BYTE_SIZE)]
+    bit = (byte // powi) % 2
+    if bit == 1:
+        # v == 0
+        new_byte = byte - powi
+    else:
+        # v == 1
+        new_byte = byte + powi
+    return bmp[:scaled_i] + bytes([new_byte]) + bmp[scaled_i + 1 :]
+
+
+def size_bitmap(bmp: BitMap) -> int:
+    """Returns the size of the bitmap in bits"""
+    return len(bmp) * BYTE_SIZE
+
+
+def any_bitmap(b: BitMap) -> int:
+    """Returns whether any bit was set to 1"""
+    return b != (b"\x00" * len(b))
+
+
+def all_bitmap(b: BitMap) -> int:
+    """Returns whether all bits were set to 1"""
+    return b == (b"\xFF" * len(b))
+
+
+def none_bitmap(b: BitMap) -> int:
+    """Returns whether no bits were set to 1"""
+    return b == (b"\x00" * len(b))
--- opshin/tests/test_std/test_bitmap.py
+++ opshin/tests/test_std/test_bitmap.py
@@ -21,18 +21,68 @@ def bytes_and_index(draw):
 
 
 @hypothesis.given(bytes_and_index())
-def test_isset_bitmap(p: Tuple[bytes, int]):
+def test_test_bitmap(p: Tuple[bytes, int]):
     a, i = p
     ith_bit = int.from_bytes(a, "big") & (1 << ((len(a) * 8) - i - 1))
-    assert oc_bitmap.isset_bitmap(a, i) == (ith_bit != 0), "Invalid isset check"
+    assert oc_bitmap.test_bitmap(a, i) == (ith_bit != 0), "Invalid isset check"
 
 
 @hypothesis.given(bytes_and_index(), hst.booleans())
-def test_set_bitmap(p: Tuple[bytes, int], v: bool):
+def test__set_bitmap(p: Tuple[bytes, int], v: bool):
     a, i = p
     if v:
         set_bit = int.from_bytes(a, "big") | (1 << ((len(a) * 8) - i - 1))
     else:
         set_bit = int.from_bytes(a, "big") & (~(1 << ((len(a) * 8) - i - 1)))
     set_bit = set_bit.to_bytes(len(a), byteorder="big")
-    assert oc_bitmap.set_bitmap(a, i, v) == set_bit, "Set bit incorrectly"
+    assert oc_bitmap._set_bitmap(a, i, v) == set_bit, "Set bit incorrectly"
+
+
+@hypothesis.given(bytes_and_index())
+def test_set_bitmap(p: Tuple[bytes, int]):
+    a, i = p
+    set_bit = int.from_bytes(a, "big") | (1 << ((len(a) * 8) - i - 1))
+    set_bit = set_bit.to_bytes(len(a), byteorder="big")
+    assert oc_bitmap.set_bitmap(a, i) == set_bit, "Set bit incorrectly"
+
+
+@hypothesis.given(bytes_and_index())
+def test_reset_bitmap(p: Tuple[bytes, int]):
+    a, i = p
+    set_bit = int.from_bytes(a, "big") & (~(1 << ((len(a) * 8) - i - 1)))
+    set_bit = set_bit.to_bytes(len(a), byteorder="big")
+    assert oc_bitmap.reset_bitmap(a, i) == set_bit, "Reset bit incorrectly"
+
+
+@hypothesis.given(bytes_and_index())
+def test_flip_bitmap(p: Tuple[bytes, int]):
+    a, i = p
+    set_bit = int.from_bytes(a, "big") ^ (1 << ((len(a) * 8) - i - 1))
+    set_bit = set_bit.to_bytes(len(a), byteorder="big")
+    assert oc_bitmap.flip_bitmap(a, i) == set_bit, "Flipped bit incorrectly"
+
+
+@hypothesis.given(hst.binary())
+def test_size_bitmap(x: bytes):
+    assert len(x) * 8 == oc_bitmap.size_bitmap(x), "Incorrect size of bitmap"
+
+
+@hypothesis.given(hst.binary())
+def test_all_bitmap(x: bytes):
+    assert oc_bitmap.all_bitmap(x) == all(
+        b == 0xFF for b in x
+    ), "All is incorrect for bitmap"
+
+
+@hypothesis.given(hst.binary())
+def test_any_bitmap(x: bytes):
+    assert oc_bitmap.any_bitmap(x) == any(
+        b != 0x00 for b in x
+    ), "Any is incorrect for bitmap"
+
+
+@hypothesis.given(hst.binary())
+def test_none_bitmap(x: bytes):
+    assert oc_bitmap.none_bitmap(x) == all(
+        b == 0x00 for b in x
+    ), "None is incorrect for bitmap"
