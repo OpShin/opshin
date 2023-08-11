3ce2e0f8b5d00ff1ab6f97afc7c6ffb36ab189c5
Niels Mündler <n.muendler@posteo.de>
2023-08-12 01:29:45 +0200

    Beef up example

--- opshin/tests/test_misc.py
+++ opshin/tests/test_misc.py
@@ -2315,17 +2315,29 @@ def validator(
         code = compiler.compile(ast)
         code = code.compile()
 
-    @hypothesis.given(st.lists(st.integers(), max_size=10, min_size=2))
+    @hypothesis.given(
+        st.lists(
+            st.tuples(
+                st.integers(), st.sampled_from(["<", "<=", "==", ">=", ">", "!="])
+            ),
+            max_size=10,
+            min_size=2,
+        )
+    )
     def test_comparison_chaining(self, xs):
         param_string = ",".join(f"i{k}: int" for k, _ in enumerate(xs))
-        comp_string = " < ".join(f"i{k}" for k, _ in enumerate(xs))
+        comp_string = "i0"
+        eval_string = f"{xs[0][0]}"
+        for k, (x, c) in enumerate(xs[1:], start=1):
+            comp_string += f" {c} i{k}"
+            eval_string += f" {c} {x}"
         source_code = f"""
 def validator({param_string}) -> bool:
     return {comp_string}
 """
         ast = compiler.parse(source_code)
         code = compiler.compile(ast).compile()
-        for x in xs:
+        for x, _ in xs:
             code = uplc.Apply(code, uplc.PlutusInteger(x))
         res = uplc_eval(code)
-        self.assertEqual(bool(res.value), eval(" < ".join(str(x) for x in xs)))
+        self.assertEqual(bool(res.value), eval(eval_string))
