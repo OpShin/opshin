e1ac6ec80142d7755666433ac95d507c1c97ea3d
Niels Mündler <n.muendler@posteo.de>
2023-09-04 09:45:55 +0200

    Add test for list integrity check

--- opshin/tests/test_std/test_integrity.py
+++ opshin/tests/test_std/test_integrity.py
@@ -37,3 +37,60 @@ def validator(x: B) -> None:
     else:
         res = True
     assert res == (len(xs) == 2)
+
+
+@parameterized.expand(
+    [
+        [[0, 1], [1, 1, 1]],
+        [[b"hello"], [1, 1]],
+        [[0, 1, 2], [1, 0]],
+    ]
+)
+def test_integrity_check_list(foobar, bar_constrs):
+    source_code = """
+from dataclasses import dataclass
+from typing import Dict, List, Union
+from pycardano import Datum as Anything, PlutusData
+from opshin.std.integrity import check_integrity
+
+@dataclass()
+class A(PlutusData):
+    CONSTR_ID = 1
+
+@dataclass()
+class B(PlutusData):
+    CONSTR_ID = 1
+    foobar: List[int]
+    bar: List[A]
+
+def validator(x: B) -> None:
+    check_integrity(x)
+"""
+    ast = compiler.parse(source_code)
+    code = compiler.compile(ast).compile()
+    code = uplc.Apply(
+        code,
+        uplc.PlutusConstr(
+            1,
+            [
+                uplc.PlutusList(
+                    [
+                        uplc.PlutusInteger(x)
+                        if isinstance(x, int)
+                        else uplc.PlutusByteString(x)
+                        for x in foobar
+                    ]
+                ),
+                uplc.PlutusList([uplc.PlutusConstr(c, []) for c in bar_constrs]),
+            ],
+        ),
+    )
+    try:
+        uplc_eval(code)
+    except:
+        res = False
+    else:
+        res = True
+    assert res == (
+        all(isinstance(x, int) for x in foobar) and all(c == 1 for c in bar_constrs)
+    )
