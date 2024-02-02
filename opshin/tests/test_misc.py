887417dc8ebba1f2672e64652b4bd49424942cd4
Niels Mündler <n.muendler@posteo.de>
2024-02-02 10:26:04 +0100

    Discover a case where the program allows invalid types
    This happens because the name of captured variables is the same but the
    type is not. The type is merge after the branch but not checked to be
    compatible with the function type.
    
    A simple fix should disallow merging the functions because they capture
    variables of different types.

--- opshin/tests/test_misc.py
+++ opshin/tests/test_misc.py
@@ -2776,3 +2776,34 @@ def validator(x: Union[A, B, C]) -> int:
     return fun(x)
         """
         builder._compile(source_code)
+
+    @unittest.expectedFailure
+    def test_merge_function_same_capture_different_type(self):
+        source_code = """
+from typing import Dict, List, Union
+from pycardano import Datum as Anything, PlutusData
+from dataclasses import dataclass
+
+@dataclass()
+class A(PlutusData):
+    CONSTR_ID = 0
+    foo: int
+
+@dataclass()
+class B(PlutusData):
+    CONSTR_ID = 1
+    bar: int
+
+def validator(x: bool) -> int:
+    if x:
+        y = A(0)
+        def foo() -> int:
+            return y.foo
+    else:
+        y = B(0)
+        def foo() -> int:
+            return y.bar
+    y = A(0)
+    return foo()
+        """
+        builder._compile(source_code)
