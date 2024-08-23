ca2debb58bc671d3cc1643f39ab5e65b2d199c22
Niels <niels.muendler@inf.ethz.ch>
2024-08-23 11:59:39 +0200

    Fix running pdoc in poetry

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 set -e
 
-pip install pdoc3
 rm -r docs/opshin || echo "docs/opshin was missing, that's ok"
-pdoc --html opshin -o docs --template-dir docs
+poetry run pdoc --html opshin -o docs --template-dir docs
