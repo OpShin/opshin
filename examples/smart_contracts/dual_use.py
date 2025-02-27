b401e306b6b9fafcc11fd8443e25158bd0052220
Niels <niels.muendler@inf.ethz.ch>
2025-02-27 23:04:16 +0100

    Fix dual use sample

--- examples/smart_contracts/dual_use.py
+++ examples/smart_contracts/dual_use.py
@@ -4,8 +4,9 @@ from opshin.prelude import *
 """ This contract allows both minting and spending from its address """
 
 
-# this contract should always be called with three virtual parameters, so enable --force-three-params
-#
+# this contract should always be built with three virtual parameters, setting --force-three-params.
+# For example:
 # $ opshin build spending examples/smart_contracts/dual_use.py --force-three-params
+# Note that dual-use contracts need to have a Union[Nothing] datum (it is nothing for anything but spending) and a non-0 constructor for redeemers
 def validator(_: Nothing, r: int, ctx: ScriptContext) -> None:
     assert r == 42, "Wrong redeemer"
