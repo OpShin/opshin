b236bd8f13c1b1a5231124ca684a8a351178e538
Niels <niels.muendler@inf.ethz.ch>
2025-08-10 09:58:59 +0200

    Raise an error when importing in the wrong format

---
+++ tests/test_rewrite/test_import.py
@@ -0,0 +1,29 @@
+from opshin import builder, CompilerError
+
+
+def test_import_without_from():
+    source_code = """
+import opshin.prelude
+
+def validator(a: int) -> int:
+    return opshin.prelude.add(a, 1)
+    """
+    try:
+        builder._compile(source_code)
+        assert False, "Expected compilation failure due to import without from"
+    except CompilerError as e:
+        assert "import" in str(e).lower(), "Unexpected error message"
+
+
+def test_import_without_from_with_as():
+    source_code = """
+import opshin.prelude as prelude
+
+def validator(a: int) -> int:
+    return prelude.add(a, 1)
+    """
+    try:
+        builder._compile(source_code)
+        assert False, "Expected compilation failure due to import without from"
+    except CompilerError as e:
+        assert "import" in str(e).lower(), "Unexpected error message"
