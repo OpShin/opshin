c08252254d5797307d5dd59e7f4f378ca8f4eccf
Niels <niels.muendler@inf.ethz.ch>
2025-08-10 09:52:19 +0200

    Refactor test structure

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,6 +1,5 @@
 import os
 
-import pycardano
 import sys
 
 import subprocess
@@ -12,14 +11,14 @@ import unittest
 import frozendict
 import frozenlist2
 import hypothesis
-from hypothesis import given, example
+from hypothesis import given
 from hypothesis import strategies as st
 from parameterized import parameterized
 
-from uplc import ast as uplc, eval as uplc_eval
+from uplc import ast as uplc
 
 from . import PLUTUS_VM_PROFILE
-from opshin import prelude, builder, Purpose, PlutusContract, CompilerError
+from opshin import prelude, builder, Purpose, PlutusContract
 from .utils import eval_uplc_value, Unit, eval_uplc, eval_uplc_raw
 from opshin.bridge import wraps_builtin
 from opshin.compiler_config import OPT_O2_CONFIG, DEFAULT_CONFIG
@@ -28,11 +27,8 @@ hypothesis.settings.load_profile(PLUTUS_VM_PROFILE)
 
 # these imports are required to eval the result of script context dumps
 from opshin.ledger.api_v2 import *
-from pycardano import RawPlutusData, RawCBOR
-from cbor2 import CBORTag
 
 DEFAULT_CONFIG_FORCE_THREE_PARAMS = DEFAULT_CONFIG.update(force_three_params=True)
-DEFAULT_CONFIG_CONSTANT_FOLDING = DEFAULT_CONFIG.update(constant_folding=True)
 
 ALL_EXAMPLES = [
     os.path.join(root, f)
@@ -983,60 +979,6 @@ def validator(a) -> bytes:
         res = eval_uplc(source_code, b"")
         self.assertEqual(res, uplc.PlutusByteString(b""))
 
-    @given(xs=st.dictionaries(st.integers(), st.binary()))
-    def test_dict_items_values_deconstr(self, xs):
-        # asserts that deconstruction of parameters works for for loops too
-        source_code = """
-def validator(xs: Dict[int, bytes]) -> bytes:
-    sum_values = b""
-    for _, x in xs.items():
-        sum_values += x
-    return sum_values
-"""
-        ret = eval_uplc_value(source_code, xs)
-        self.assertEqual(
-            ret,
-            b"".join(xs.values()),
-            "for loop deconstruction did not behave as expected",
-        )
-
-    def test_nested_deconstruction(self):
-        source_code = """
-def validator(xs) -> int:
-    a, ((b, c), d) = (1, ((2, 3), 4))
-    return a + b + c + d
-"""
-        ret = eval_uplc_value(source_code, Unit())
-        self.assertEqual(
-            ret,
-            1 + 2 + 3 + 4,
-            "for loop deconstruction did not behave as expected",
-        )
-
-    @given(
-        xs=st.dictionaries(
-            st.binary(),
-            st.dictionaries(st.binary(), st.integers(), max_size=3),
-            max_size=5,
-        )
-    )
-    def test_dict_items_values_deconstr(self, xs):
-        # nested deconstruction with a Value-like object
-        source_code = """
-def validator(xs: Dict[bytes, Dict[bytes, int]]) -> int:
-    sum_values = 0
-    for pid, tk_dict in xs.items():
-        for tk_name, tk_amount in tk_dict.items():
-            sum_values += tk_amount
-    return sum_values
-"""
-        ret = eval_uplc_value(source_code, xs)
-        self.assertEqual(
-            ret,
-            sum(v for pid, d in xs.items() for nam, v in d.items()),
-            "for loop deconstruction did not behave as expected",
-        )
-
     def test_no_return_annotation_no_return(self):
         source_code = """
 from opshin.prelude import *
@@ -1059,249 +1001,6 @@ def validator(x: Token) -> bool:
         """
         ret = eval_uplc(source_code, Unit())
 
-    def test_constant_folding(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> bytes:
-    return bytes.fromhex("0011")
-"""
-        res = eval_uplc_value(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertEqual(res, bytes.fromhex("0011"))
-
-    @unittest.expectedFailure
-    def test_constant_folding_disabled(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> bytes:
-    return bytes.fromhex("0011")
-"""
-        eval_uplc(source_code, Unit(), constant_folding=False)
-
-    def test_constant_folding_list(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> List[int]:
-    return list(range(0, 10, 2))
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con (list integer) [0, 2, 4, 6, 8])", code.dumps())
-        res = builder.uplc_eval(code)
-        self.assertEqual(
-            res.result,
-            uplc.PlutusList([uplc.PlutusInteger(i) for i in range(0, 10, 2)]),
-        )
-
-    def test_constant_folding_dict(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> Dict[str, bool]:
-    return {"s": True, "m": False}
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn(
-            "(con (list (pair data data)) [(B #73, I 1), (B #6d, I 0)])", code.dumps()
-        )
-        res = uplc_eval(code).result
-        if isinstance(res, Exception):
-            raise res
-        self.assertEqual(
-            res,
-            uplc.PlutusMap(
-                {
-                    uplc.PlutusByteString("s".encode()): uplc.PlutusInteger(1),
-                    uplc.PlutusByteString("m".encode()): uplc.PlutusInteger(0),
-                }
-            ),
-        )
-
-    def test_constant_folding_complex(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> Dict[str, List[Dict[bytes, int]]]:
-    return {"s": [{b"": 0}, {b"0": 1}]}
-"""
-        res = eval_uplc(source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-        self.assertEqual(
-            res,
-            uplc.PlutusMap(
-                {
-                    uplc.PlutusByteString("s".encode()): uplc.PlutusList(
-                        [
-                            uplc.PlutusMap(
-                                {uplc.PlutusByteString(b""): uplc.PlutusInteger(0)}
-                            ),
-                            uplc.PlutusMap(
-                                {uplc.PlutusByteString(b"0"): uplc.PlutusInteger(1)}
-                            ),
-                        ]
-                    ),
-                }
-            ),
-        )
-
-    def test_constant_folding_plutusdata(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> PubKeyCredential:
-    return PubKeyCredential(bytes.fromhex("0011"))
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con data (Constr 0 [B #0011]))", code.dumps())
-        res = uplc_eval(code)
-        self.assertEqual(
-            res.result,
-            uplc.PlutusConstr(
-                constructor=0, fields=[uplc.PlutusByteString(value=b"\x00\x11")]
-            ),
-        )
-
-    def test_constant_folding_user_def(self):
-        source_code = """
-def fib(i: int) -> int:
-    return i if i < 2 else fib(i-1) + fib(i-2)
-
-def validator(_: None) -> int:
-    return fib(10)
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con integer 55)", code.dumps())
-        res = uplc_eval(code).result
-        if isinstance(res, Exception):
-            raise res
-        self.assertEqual(
-            res.value,
-            55,
-        )
-
-    @unittest.expectedFailure
-    def test_constant_folding_ifelse(self):
-        source_code = """
-def validator(_: None) -> int:
-    if False:
-        a = 10
-    return a
-"""
-        eval_uplc(source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    @unittest.expectedFailure
-    def test_constant_folding_for(self):
-        source_code = """
-def validator(x: List[int]) -> int:
-    for i in x:
-        a = 10
-    return a
-"""
-        eval_uplc(source_code, [], config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    @unittest.expectedFailure
-    def test_constant_folding_for_target(self):
-        source_code = """
-def validator(x: List[int]) -> int:
-    for i in x:
-        a = 10
-    return i
-"""
-        eval_uplc(source_code, [], config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    @unittest.expectedFailure
-    def test_constant_folding_while(self):
-        source_code = """
-def validator(_: None) -> int:
-    while False:
-        a = 10
-    return a
-"""
-        eval_uplc(source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    @unittest.skip("Fine from a guarantee perspective, but needs better inspection")
-    def test_constant_folding_guaranteed_branch(self):
-        source_code = """
-def validator(_: None) -> int:
-    if False:
-        a = 20
-        b = 2 * a
-    else:
-        b = 2
-    return b
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con integer 40)", code.dumps())
-
-    @unittest.skip("Fine from a guarantee perspective, but needs better inspection")
-    def test_constant_folding_scoping(self):
-        source_code = """
-a = 4
-def validator(_: None) -> int:
-    a = 2
-    b = 5 * a
-    return b
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con integer 10)", code.dumps())
-
-    @unittest.skip("Fine from a guarantee perspective, but needs better inspection")
-    def test_constant_folding_no_scoping(self):
-        source_code = """
-def validator(_: None) -> int:
-    a = 4
-    a = 2
-    b = 5 * a
-    return b
-"""
-        code = builder._compile(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertIn("(con integer 10)", code.dumps())
-
-    def test_constant_folding_repeated_assign(self):
-        source_code = """
-def validator(i: int) -> int:
-    a = 4
-    for k in range(i):
-        a = 2
-    return a
-"""
-        code = builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-        res = uplc_eval(code, uplc.PlutusInteger(0)).result
-        if isinstance(res, Exception):
-            raise res
-        self.assertEqual(res.value, 4)
-        res = uplc_eval(code, uplc.PlutusInteger(1)).result
-        if isinstance(res, Exception):
-            raise res
-        self.assertEqual(res.value, 2)
-
-    def test_constant_folding_math(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> int:
-    return 2 ** 10
-"""
-        code = builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-        code_src = code.dumps()
-        self.assertIn(f"(con integer {2**10})", code_src)
-
     def test_reassign_builtin(self):
         source_code = """
 b = int
@@ -1322,43 +1021,6 @@ def validator(_: None) -> int:
     return int(5)
 """
         builder._compile(source_code)
-
-    def test_constant_folding_ignore_reassignment(self):
-        source_code = """
-b = int
-def validator(_: None) -> int:
-    def int(a) -> b:
-        return 2
-    return int(5)
-"""
-        res = eval_uplc_value(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertEqual(res, 2)
-
-    def test_constant_folding_no_print_eval(self):
-        source_code = """
-from opshin.prelude import *
-
-def validator(_: None) -> None:
-    return print("hello")
-"""
-        code = builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-        code_src = code.dumps()
-        self.assertIn(f'(con string "hello")', code_src)
-
-    def test_inner_outer_state_functions(self):
-        source_code = """
-a = 2
-def b() -> int:
-    return a
-
-def validator(_: None) -> int:
-    a = 3
-    return b()
-        """
-        res = eval_uplc_value(source_code, Unit())
-        self.assertEqual(res, 2)
 
     def test_inner_outer_state_functions_nonglobal(self):
         source_code = """
@@ -2248,87 +1910,6 @@ def validator({param_string}) -> bool:
         res = eval_uplc_value(source_code, *[x[0] for x in xs])
         self.assertEqual(bool(res), eval(eval_string))
 
-    def test_double_import_offset(self):
-        source_code = """
-from opshin.ledger.api_v2 import *
-from opshin.prelude import *
-
-def validator(
-    d: Nothing,
-    r: Nothing,
-    context: ScriptContext,
-):
-    house_address = Address(
-        payment_credential=PubKeyCredential(
-            credential_hash=b""
-        ),
-        staking_credential=SomeStakingCredential(
-            staking_credential=StakingHash(
-                value=PubKeyCredential(
-                    credential_hash=b""
-                )
-            )
-        ),
-    )
-"""
-        # would fail because Address is assigned multiple times and then not constant folded
-        # TODO find a better way
-        builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    def test_double_import_direct(self):
-        source_code = """
-from opshin.prelude import *
-from opshin.prelude import *
-
-def validator(
-    d: Nothing,
-    r: Nothing,
-    context: ScriptContext,
-):
-    house_address = Address(
-        payment_credential=PubKeyCredential(
-            credential_hash=b""
-        ),
-        staking_credential=SomeStakingCredential(
-            staking_credential=StakingHash(
-                value=PubKeyCredential(
-                    credential_hash=b""
-                )
-            )
-        ),
-    )
-"""
-        # would fail because Address is assigned multiple times and then not constant folded
-        # TODO find a better way
-        builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
-    def test_double_import_deep(self):
-        source_code = """
-from opshin.ledger.interval import *
-from opshin.prelude import *
-
-def validator(
-    d: Nothing,
-    r: Nothing,
-    context: ScriptContext,
-):
-    house_address = Address(
-        payment_credential=PubKeyCredential(
-            credential_hash=b""
-        ),
-        staking_credential=SomeStakingCredential(
-            staking_credential=StakingHash(
-                value=PubKeyCredential(
-                    credential_hash=b""
-                )
-            )
-        ),
-    )
-"""
-        # would fail because Address is assigned multiple times and then not constant folded
-        # TODO find a better way
-        builder._compile(source_code, config=DEFAULT_CONFIG_CONSTANT_FOLDING)
-
     def test_bytearray_alternative(self):
         source_code = """
 def validator(
@@ -2669,17 +2250,6 @@ def validator(_: None) -> List[Token]:
             ],
         )
 
-    def test_empty_list_int_constant_folding(self):
-        source_code = """
-def validator(_: None) -> List[int]:
-    a: List[int] = []
-    return a
-"""
-        res = eval_uplc_value(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertEqual(res, [])
-
     def test_empty_dict_int_int(self):
         source_code = """
 def validator(_: None) -> Dict[int, int]:
@@ -2687,32 +2257,6 @@ def validator(_: None) -> Dict[int, int]:
     return a
 """
         res = eval_uplc_value(source_code, Unit())
-        self.assertEqual(res, {})
-
-    def test_empty_dict_int_int_constant_folding(self):
-        source_code = """
-def validator(_: None) -> Dict[int, int]:
-    a: Dict[int, int] = {}
-    return a
-"""
-        res = eval_uplc_value(
-            source_code, Unit(), config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
-        self.assertEqual(res, {})
-
-    def test_empty_dict_displaced_constant_folding(self):
-        source_code = """
-from typing import Dict, List, Union
-
-VAR: Dict[bytes, int] = {}
-
-def validator(b: Dict[int, Dict[bytes, int]]) -> Dict[bytes, int]:
-    a = b.get(0, VAR)
-    return a
-        """
-        res = eval_uplc_value(
-            source_code, {1: {b"": 0}}, config=DEFAULT_CONFIG_CONSTANT_FOLDING
-        )
         self.assertEqual(res, {})
 
     def test_union_subset_call(self):
--- tests/test_rewrite/test_tuple_assign.py
+++ tests/test_rewrite/test_tuple_assign.py
@@ -1,6 +1,10 @@
 import unittest
 
+from hypothesis import given
+from hypothesis import strategies as st
+
 from opshin import builder, CompilerError
+from tests.utils import eval_uplc_value, Unit
 
 
 class TupleAssignTest(unittest.TestCase):
@@ -131,3 +135,57 @@ def validator(a: int) -> int:
                 str(e).lower(),
                 "Unexpected error message",
             )
+
+    @given(xs=st.dictionaries(st.integers(), st.binary()))
+    def test_dict_items_values_deconstr(self, xs):
+        # asserts that deconstruction of parameters works for for loops too
+        source_code = """
+def validator(xs: Dict[int, bytes]) -> bytes:
+    sum_values = b""
+    for _, x in xs.items():
+        sum_values += x
+    return sum_values
+"""
+        ret = eval_uplc_value(source_code, xs)
+        self.assertEqual(
+            ret,
+            b"".join(xs.values()),
+            "for loop deconstruction did not behave as expected",
+        )
+
+    def test_nested_deconstruction(self):
+        source_code = """
+def validator(xs) -> int:
+    a, ((b, c), d) = (1, ((2, 3), 4))
+    return a + b + c + d
+"""
+        ret = eval_uplc_value(source_code, Unit())
+        self.assertEqual(
+            ret,
+            1 + 2 + 3 + 4,
+            "for loop deconstruction did not behave as expected",
+        )
+
+    @given(
+        xs=st.dictionaries(
+            st.binary(),
+            st.dictionaries(st.binary(), st.integers(), max_size=3),
+            max_size=5,
+        )
+    )
+    def test_dict_items_values_deconstr(self, xs):
+        # nested deconstruction with a Value-like object
+        source_code = """
+def validator(xs: Dict[bytes, Dict[bytes, int]]) -> int:
+    sum_values = 0
+    for pid, tk_dict in xs.items():
+        for tk_name, tk_amount in tk_dict.items():
+            sum_values += tk_amount
+    return sum_values
+"""
+        ret = eval_uplc_value(source_code, xs)
+        self.assertEqual(
+            ret,
+            sum(v for pid, d in xs.items() for nam, v in d.items()),
+            "for loop deconstruction did not behave as expected",
+        )
