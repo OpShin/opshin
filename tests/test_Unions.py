a04cfa9e1b062758c856815ea660e44d1b6a05ee
Niels <niels.muendler@inf.ethz.ch>
2025-09-03 09:47:25 +0200

    Add tests for union expansion

--- tests/test_Unions.py
+++ tests/test_Unions.py
@@ -618,7 +618,7 @@ def validator(x: int) -> int:
         self.assertEqual(source.cost.memory, target.cost.memory)
 
     @hypothesis.given(st.sampled_from(range(4, 7)))
-    def test_Union_expansion_BoolOp(self, x):
+    def test_Union_expansion_BoolOp_and(self, x):
         source_code = """
 from typing import Dict, List, Union
 
@@ -642,6 +642,113 @@ def foo(x: int) -> int:
         k = x +1
     else:
         k = x - 1
+    return k
+
+def validator(x: int) -> int:
+    return foo(x)
+"""
+        config = DEFAULT_CONFIG
+        euo_config = config.update(expand_union_types=True)
+        source = eval_uplc_raw(source_code, x, config=euo_config)
+        target = eval_uplc_raw(target_code, x, config=config)
+
+        self.assertEqual(source.result, target.result)
+        self.assertEqual(source.cost.cpu, target.cost.cpu)
+        self.assertEqual(source.cost.memory, target.cost.memory)
+
+    @hypothesis.given(st.sampled_from(range(4, 7)))
+    def test_Union_expansion_BoolOp_or(self, x):
+        source_code = """
+from typing import Dict, List, Union
+
+def foo(x: Union[int, bytes]) -> int:
+    if isinstance(x, bytes) or x == 2:
+        k = 2
+    elif isinstance(x, int):
+        k = x - 1
+    else:
+        k = len(x)
+    return k
+
+def validator(x: int) -> int:
+    return foo(x)
+"""
+        target_code = """
+from typing import Dict, List, Union
+
+def foo(x: int) -> int:
+    if x == 2:
+        k = x +1
+    else:
+        k = x - 1
+    return k
+
+def validator(x: int) -> int:
+    return foo(x)
+"""
+        config = DEFAULT_CONFIG
+        euo_config = config.update(expand_union_types=True)
+        source = eval_uplc_raw(source_code, x, config=euo_config)
+        target = eval_uplc_raw(target_code, x, config=config)
+
+        self.assertEqual(source.result, target.result)
+        self.assertEqual(source.cost.cpu, target.cost.cpu)
+        self.assertEqual(source.cost.memory, target.cost.memory)
+
+    @hypothesis.given(st.sampled_from([b"123", b"1"]), st.sampled_from([b"123", b"1"]))
+    def test_Union_expansion_BoolOp_and_all(self, x, y):
+        source_code = """
+from typing import Dict, List, Union
+
+def foo(x: Union[int, bytes], y: Union[int, bytes]) -> int:
+    if isinstance(x, bytes) and isinstance(y, bytes):
+        k = len(x) + len(y)
+    else:
+        k = 2
+    return k
+
+def validator(x: bytes, y: bytes) -> int:
+    return foo(x, y)
+"""
+        target_code = """
+from typing import Dict, List, Union
+
+def foo(x: bytes, y: bytes) -> int:
+    k = len(x) + len(y)
+    return k
+
+def validator(x: bytes, y: bytes) -> int:
+    return foo(x, y)
+"""
+        config = DEFAULT_CONFIG
+        euo_config = config.update(expand_union_types=True)
+        source = eval_uplc_raw(source_code, x, y, config=euo_config)
+        target = eval_uplc_raw(target_code, x, y, config=config)
+
+        self.assertEqual(source.result, target.result)
+        self.assertEqual(source.cost.cpu, target.cost.cpu)
+        self.assertEqual(source.cost.memory, target.cost.memory)
+
+    @hypothesis.given(st.sampled_from(range(4, 7)))
+    def test_Union_expansion_BoolOp_or_all(self, x):
+        source_code = """
+from typing import Dict, List, Union
+
+def foo(x: Union[int, bytes]) -> int:
+    if isinstance(x, bytes) or isinstance(x, int):
+        k = 2
+    else:
+        k = len(x)
+    return k
+
+def validator(x: int) -> int:
+    return foo(x)
+"""
+        target_code = """
+from typing import Dict, List, Union
+
+def foo(x: int) -> int:
+    k = 2
     return k
 
 def validator(x: int) -> int:
