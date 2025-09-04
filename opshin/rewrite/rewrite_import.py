e3c013fab7a6132be02293d0a79c342c6da5fb90
Niels <niels.muendler@inf.ethz.ch>
2025-09-04 19:07:35 +0200

    Catch errors when importing invalid modules

--- opshin/rewrite/rewrite_import.py
+++ opshin/rewrite/rewrite_import.py
@@ -24,16 +24,23 @@ def import_module(name, package=None):
         pass
 
     path = None
-    if "." in absolute_name:
-        parent_name, _, child_name = absolute_name.rpartition(".")
-        parent_module = import_module(parent_name)
-        path = parent_module.__spec__.submodule_search_locations
-    for finder in sys.meta_path:
-        spec = finder.find_spec(absolute_name, path)
-        if spec is not None:
-            break
-    else:
+    spec = None
+    error_msg = None
+    try:
+        if "." in absolute_name:
+            parent_name, _, child_name = absolute_name.rpartition(".")
+            parent_module = import_module(parent_name)
+            path = parent_module.__spec__.submodule_search_locations
+        for finder in sys.meta_path:
+            spec = finder.find_spec(absolute_name, path)
+            if spec is not None:
+                break
+    except (ImportError, AttributeError) as e:
+        error_msg = str(e)
+    if spec is None:
         msg = f"No module named {absolute_name!r}"
+        if error_msg:
+            msg += f"; {error_msg}"
         raise ModuleNotFoundError(msg, name=absolute_name)
     module = importlib.util.module_from_spec(spec)
     sys.modules[absolute_name] = module
