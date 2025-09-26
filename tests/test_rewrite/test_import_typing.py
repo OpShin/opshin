e9191c33c0eee75898e229ad083ce93210faa2db
Niels <niels.muendler@inf.ethz.ch>
2025-09-26 15:28:12 +0200

    Add tests

--- tests/test_rewrite/test_import_typing.py
+++ tests/test_rewrite/test_import_typing.py
@@ -1,12 +1,13 @@
 import pytest
 
 from opshin import CompilerError, builder
+from tests.utils import eval_uplc_value, eval_uplc
 
 
 def test_error_message_list_no_import():
     source_code = """
-def validator(x: List) -> None:
-    assert isinstance(x, int)
+def validator(x: List[int]) -> None:
+    assert isinstance(x[0], int)
         """
     with pytest.raises(CompilerError, match="not imported") as e:
         builder._compile(source_code)
@@ -47,3 +48,41 @@ def validator(x: A) -> None:
         """
     with pytest.raises(CompilerError, match="not imported") as e:
         builder._compile(source_code)
+
+
+def test_import_just_list():
+    source_code = """
+from typing import List
+def validator(x: List[int]) -> None:
+    assert isinstance(x[0], int)
+        """
+    eval_uplc(source_code, [1, 2, 3])
+
+
+def test_import_just_dict():
+    source_code = """
+from typing import Dict
+def validator(x: Dict[int, int]) -> None:
+    assert isinstance(x[0], int)
+        """
+    eval_uplc(source_code, {1: 1, 2: 0, 0: 1})
+
+
+def test_import_just_union():
+    source_code = """
+from typing import Union
+def validator(x: Union[int, bytes]) -> None:
+    assert isinstance(x, int)
+        """
+    eval_uplc(source_code, 1)
+    with pytest.raises(RuntimeError, match="Execution") as e:
+        eval_uplc(source_code, b"hi")
+
+
+def test_import_mix():
+    source_code = """
+from typing import Dict, List
+def validator(x: Dict[int, int], y: List[bytes]) -> None:
+    assert isinstance(x[0], int)
+        """
+    eval_uplc(source_code, {1: 1, 2: 0, 0: 1}, [b"hi"])
