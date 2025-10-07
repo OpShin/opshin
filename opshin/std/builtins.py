13bf6b890409cece406655b043213a903a98ec0f
Niels <niels.muendler@inf.ethz.ch>
2025-10-08 00:32:44 +0200

    Fix documentation

--- opshin/std/builtins.py
+++ opshin/std/builtins.py
@@ -192,32 +192,34 @@ def equals_data(x: Anything, y: Anything) -> bool:
 
 
 @wraps_builtin
-def integer_to_byte_string(a: int) -> bytes:
-    """Returns the integer represented by a bytestring."""
+def integer_to_byte_string(big_endian: bool, width: int, val: int) -> bytes:
+    """Returns the integer represented by a bytestring.
+    Width controls the number of bytes used. If width is 0, the minimum number of bytes is used.
+    Big_endian controls the endianness."""
     pass
 
 
 @wraps_builtin
-def byte_string_to_integer(a: bytes) -> int:
+def byte_string_to_integer(big_endian: bool, a: bytes) -> int:
     """Returns the representation of an integer as a bytestring. Undoes integer_to_byte_string."""
     pass
 
 
 @wraps_builtin
-def and_byte_string(a: bytes, b: bytes) -> bytes:
-    """Returns the bitwise AND of two bytestrings, asserting they have the same length."""
+def and_byte_string(pad: bool, a: bytes, b: bytes) -> bytes:
+    """Logical AND applied to two bytestrings. The first argument indicates whether padding semantics should be used. If this argument is False, truncation semantics are used instead."""
     pass
 
 
 @wraps_builtin
-def or_byte_string(a: bytes, b: bytes) -> bytes:
-    """Returns the bitwise OR of two bytestrings, asserting they have the same length."""
+def or_byte_string(pad: bool, a: bytes, b: bytes) -> bytes:
+    """Returns the bitwise OR of two bytestrings, asserting they have the same length. The first argument indicates whether padding semantics should be used. If this argument is False, truncation semantics are used instead."""
     pass
 
 
 @wraps_builtin
-def xor_byte_string(a: bytes, b: bytes) -> bytes:
-    """Returns the bitwise XOR of two bytestrings, asserting they have the same length."""
+def xor_byte_string(pad: bool, a: bytes, b: bytes) -> bytes:
+    """Logical XOR applied to two bytestrings. The first argument indicates whether padding semantics should be used. If this argument is False, truncation semantics are used instead."""
     pass
 
 
