1a9a36270f76ff098bd380f6ee154232dcf82c53
Niels <niels.muendler@inf.ethz.ch>
2025-10-19 12:34:58 +0200

    Adapt bitwise

--- examples/smart_contracts/bitwise.py
+++ examples/smart_contracts/bitwise.py
@@ -8,11 +8,18 @@ def validator(context: ScriptContext) -> None:
     """
     A contract that checks whether the bitwise AND of the datum and redeemer is zero.
     """
-    datum: int = own_datum_unsafe(context)
-    redeemer: int = context.redeemer
-    datum_bytes = bytes_big_from_unsigned_int(datum)
-    redeemer_bytes = bytes_big_from_unsigned_int(redeemer)
-    # compute the bitwise AND of the two byte arrays
-    and_bytes = and_bytestring(datum_bytes, redeemer_bytes)
-    and_int = unsigned_int_from_bytes_big(and_bytes)
-    assert and_int == 0, f"Expected bitwise AND to be zero, but got {and_int}"
+    maybe_datum = own_datum(context)
+    # it is possible that no datum is attached (new in PlutusV3)
+    if isinstance(maybe_datum, NoOutputDatum):
+        # in that case, no datum was attached and we accept the transaction
+        return
+    else:
+        datum: int = maybe_datum.datum
+        # In plutus v3, the redeemer is in the script context
+        redeemer: int = context.redeemer
+        datum_bytes = bytes_big_from_unsigned_int(datum)
+        redeemer_bytes = bytes_big_from_unsigned_int(redeemer)
+        # compute the bitwise XOR of the two byte arrays
+        and_bytes = xor_byte_string(True, datum_bytes, redeemer_bytes)
+        and_int = unsigned_int_from_bytes_big(and_bytes)
+        assert and_int == 0, f"Expected bitwise XOR to be zero, but got {and_int}"
